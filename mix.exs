e9ffa8b107d2679ccd1f16a627681ad72a16d1f2
amalbuquerque <andre.m.de.albuquerque@gmail.com>
2018-09-29 11:38:04 +0100

    0.1.0 release with mix

--- mix.exs
+++ mix.exs
@@ -4,6 +4,8 @@ defmodule Interceptor.MixProject do
   def project do
     [
       app: :interceptor,
+      package: package(),
+      source_url: "https://github.com/amalbuquerque/interceptor",
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
@@ -22,6 +24,7 @@ defmodule Interceptor.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
@@ -29,4 +32,15 @@ defmodule Interceptor.MixProject do
 
   defp elixirc_paths(:test), do: ["lib", "test/intercepted_modules"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp package() do
+    [
+      description: "Library to easily intercept function calls",
+      licenses: ["MIT"],
+      maintainers: ["andre.malbuq@gmail.com"],
+      links: %{
+        Github: "https://github.com/amalbuquerque/interceptor"
+      },
+    ]
+  end
 end
