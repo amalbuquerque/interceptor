a7405b30684617067e2a49937899db33f4ab8f2d
amalbuquerque <andre.m.de.albuquerque@gmail.com>
2018-09-30 13:31:57 +0100

    New logo + updated README.md

--- mix.exs
+++ mix.exs
@@ -6,27 +6,27 @@ defmodule Interceptor.MixProject do
       app: :interceptor,
       package: package(),
       source_url: "https://github.com/amalbuquerque/interceptor",
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      elixirc_paths: elixirc_paths(Mix.env())
+      elixirc_paths: elixirc_paths(Mix.env()),
+      docs: [main: "Interceptor",
+        logo: "assets/images/interceptor_logo_small.png",
+        assets: "assets"
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:ex_doc, ">= 0.0.0", only: :dev},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 
