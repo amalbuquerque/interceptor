c028ed1c2c09d7b72e98328c2d5505bedca9f153
amalbuquerque <andre.m.de.albuquerque@gmail.com>
2018-10-28 21:58:30 +0000

    Interceptor.Utils exposes `get_mfa_from_string/1` and has tests

---
+++ test/utils_test.exs
@@ -0,0 +1,40 @@
+defmodule InterceptorUtilsTest do
+  use ExUnit.Case
+
+  @subject Interceptor.Utils
+
+  describe "random_atom/1" do
+    test "it returns an atom of the passed size" do
+      result = @subject.random_atom(42)
+
+      assert result |> to_string() |> String.length() == 42
+      assert is_atom(result)
+    end
+  end
+
+  describe "random_string/1" do
+    test "it returns a string of the passed size" do
+      result = @subject.random_string(42)
+
+      assert String.length(result) == 42
+      assert is_binary(result)
+    end
+  end
+
+  describe "get_mfa_from_string/1" do
+    test "it returns the MFA of a simple module" do
+      assert {TheCoolModule, :func, 0} == @subject.get_mfa_from_string("TheCoolModule.func/0")
+    end
+
+    test "it returns the MFA of a module that belongs to a big namespace" do
+      assert {Zi.Zo.Mananananana.Foo.Bar.Qaz.Zac.Bla, :func, 0} == @subject.get_mfa_from_string("Zi.Zo.Mananananana.Foo.Bar.Qaz.Zac.Bla.func/0")
+    end
+
+    test "it returns the MFA of an existing function" do
+      {module, func, arity} = @subject.get_mfa_from_string("Enum.member?/2")
+
+      assert {Enum, :member?, 2} == {module, func, arity}
+      assert function_exported?(module, func, arity)
+    end
+  end
+end
