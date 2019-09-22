516c384d782acce4f5dd556b7e7ae2f368d9d6ed
amalbuquerque <andre.m.de.albuquerque@gmail.com>
2019-09-23 00:00:45 +0100

    Release v0.5.1
    Let's release the fix asap. There's no breaking change.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Interceptor.MixProject do
       app: :interceptor,
       package: package(),
       source_url: "https://github.com/amalbuquerque/interceptor",
-      version: "0.5.0",
+      version: "0.5.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
