655b92942ed828c92bcb8c58f4757a87d67d4394
amalbuquerque <andre.m.de.albuquerque@gmail.com>
2019-11-02 08:13:26 +0000

    Configuration.get_interceptor_mfa_for accepts wildcarded MFAs
    Also, the validator doesn't try to validate the existence of wildcarded
    MFAs, since a function named `:*` and/or with arity `:*` doesn't usually
    exist.

--- lib/configuration/configuration.ex
+++ lib/configuration/configuration.ex
@@ -17,9 +17,11 @@ defmodule Interceptor.Configuration do
     get_interceptor_module_function_for(mfa_to_intercept, interception_type)
   end
 
-  def get_interceptor_module_function_for({module, _function, _arity} = to_intercept, interception_type) do
+  def get_interceptor_module_function_for({module, function, _arity} = to_intercept, interception_type) do
     interception_configuration = get_configuration(module)
     configuration = interception_configuration[to_intercept]
+                    || interception_configuration[{module, function, :*}]
+                    || interception_configuration[{module, :*, :*}]
 
     configuration && Keyword.get(configuration, interception_type)
   end
